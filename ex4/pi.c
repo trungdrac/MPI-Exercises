cc06dfaca30071ad899ae34b71e6d3ec62d15da7
linhbm1511 <naegawolf09@gmail.com>
2020-04-04 22:27:43 +0700

    fix: add master

--- ex4/pi.c
+++ ex4/pi.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <time.h>
 
-static long steps = 1000;
+static long steps = 1000000;
 double step;
 
 int main (int argc, const char *argv[]) {
@@ -17,7 +17,7 @@ int main (int argc, const char *argv[]) {
     omp_set_num_threads(8);
     sum = 0.0;
     start = omp_get_wtime();
-    #pragma omp parallel private(i,x)
+    #pragma omp parallel private(x)
     {
         omp_set_nested(2);
         // omp_set_dynamic(CHUNKSIZE);
@@ -27,14 +27,17 @@ int main (int argc, const char *argv[]) {
             sum += x; 
             // printf("%d: On thread %d, x is: %f\n", i, omp_get_thread_num(), x);
         }
-        printf("1. The number of processors available: %d\n", omp_get_num_procs());
-        printf("2. The number of threads being used: %d\n", omp_get_num_threads());
-        printf("3. The maximum number of threads available: %d\n", omp_get_max_threads());
-        printf("4. If being in parallel region: %d\n", omp_in_parallel());
-        printf("5. If dynamic threads are enabled: %d\n", omp_get_dynamic());
-        printf("6. If nested parallelism is supported: %d\n\n", omp_get_nested());
+        
+        #pragma omp master
+        {
+            printf("1. The number of processors available: %d\n", omp_get_num_procs());
+            printf("2. The number of threads being used: %d\n", omp_get_num_threads());
+            printf("3. The maximum number of threads available: %d\n", omp_get_max_threads());
+            printf("4. If being in parallel region: %d\n", omp_in_parallel());
+            printf("5. If dynamic threads are enabled: %d\n", omp_get_dynamic());
+            printf("6. If nested parallelism is supported: %d\n\n", omp_get_nested());
+        }
     }
-   
     delta = omp_get_wtime() - start;
     printf("PI = %.16g computed in %.6f seconds\n", sum, delta);
 }
